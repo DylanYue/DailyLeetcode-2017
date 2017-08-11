892b915af8a826504e6c641440ee5939247a225f
Dylan Yue <dingjun.yue@gmail.com>
2017-08-10 22:09:59 -0700

    Added question 566

---
+++ Python3/566_Reshape_the_Matrix.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def matrixReshape(self, nums, r, c):
+        """
+        :type nums: List[List[int]]
+        :type r: int
+        :type c: int
+        :rtype: List[List[int]]
+        """
+        if len(nums) * len(nums[0]) != r * c:
+            return nums
+        
+        vals = (val for row in nums for val in row)
+        return[[vals.next() for i in xrange(c)] for j in xrange(r)]
